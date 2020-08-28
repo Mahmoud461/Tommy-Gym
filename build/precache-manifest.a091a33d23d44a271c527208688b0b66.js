7b35735b5a8876bbdc072d4ca5f450321405e3f0
Mahmoud461 <66566293+Mahmoud461@users.noreply.github.com>
2020-08-28 15:25:42 +0200

    Add files via upload

---
+++ build/precache-manifest.a091a33d23d44a271c527208688b0b66.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244ce0a9d34230c398283fc90c43eb51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00860653a181ba5c37e4",
+    "url": "/static/css/main.8ba1d78f.chunk.css"
+  },
+  {
+    "revision": "38d53f211228ee3fed7b",
+    "url": "/static/js/2.e3db87d4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e3db87d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00860653a181ba5c37e4",
+    "url": "/static/js/main.a61b1443.chunk.js"
+  },
+  {
+    "revision": "24d4184f7e978dd608b4",
+    "url": "/static/js/runtime-main.4f396609.js"
+  },
+  {
+    "revision": "ffc2bb1ee2a10126866ace24e72c4f47",
+    "url": "/static/media/1 (1).ffc2bb1e.jpg"
+  },
+  {
+    "revision": "3617541dd27fb888b45565037e68cfaf",
+    "url": "/static/media/1 (2).3617541d.jpg"
+  },
+  {
+    "revision": "c92501966e88fb55c05296bf6080bb19",
+    "url": "/static/media/1 (3).c9250196.jpg"
+  },
+  {
+    "revision": "178f3a79ab533cccf80a4fb9ef497e9c",
+    "url": "/static/media/1 (4).178f3a79.jpg"
+  },
+  {
+    "revision": "2c984f6b61e18a5383aa1ae4c9792bf1",
+    "url": "/static/media/1 (5).2c984f6b.jpg"
+  },
+  {
+    "revision": "b45730ed714ccb6640902d5a95077b39",
+    "url": "/static/media/1 (6).b45730ed.jpg"
+  },
+  {
+    "revision": "bde93096f5146e151d11b492985feb46",
+    "url": "/static/media/CardImg.bde93096.jpg"
+  },
+  {
+    "revision": "fc56c202237aeecd8bb3b344148c4955",
+    "url": "/static/media/Clashing.fc56c202.png"
+  },
+  {
+    "revision": "68cbc7461fcf02381b96153abe75cab9",
+    "url": "/static/media/facilities1.68cbc746.jpg"
+  },
+  {
+    "revision": "0f973a2c089acf5e31798be62c209a3a",
+    "url": "/static/media/facilities2.0f973a2c.jpg"
+  },
+  {
+    "revision": "6a9e976b1299e56eaaaf6fd620690220",
+    "url": "/static/media/facilities3.6a9e976b.jpg"
+  },
+  {
+    "revision": "bd11a6cc84f94157cb71c05b61be47a1",
+    "url": "/static/media/facilities4.bd11a6cc.jpg"
+  },
+  {
+    "revision": "8e11dd3e2ef580c072335623546c1f32",
+    "url": "/static/media/facilities5.8e11dd3e.jpg"
+  },
+  {
+    "revision": "bd06fc31eadfcd0576fdb599d2c6d849",
+    "url": "/static/media/facilities6.bd06fc31.jpg"
+  },
+  {
+    "revision": "ae07f896d12491bb0ff2b0466f7f71df",
+    "url": "/static/media/icon3.ae07f896.png"
+  },
+  {
+    "revision": "3f4fead3742562d6cd6b7b0412416555",
+    "url": "/static/media/img1.3f4fead3.jpg"
+  },
+  {
+    "revision": "050ae2cff0188d8f7c2709bb121b23c3",
+    "url": "/static/media/img2.050ae2cf.jpg"
+  },
+  {
+    "revision": "373c05e649136f6fbf2f9e4798aa9b75",
+    "url": "/static/media/joinUs.373c05e6.jpg"
+  },
+  {
+    "revision": "98969e3cd6954d5b279dde4f0e5a90d2",
+    "url": "/static/media/people1.98969e3c.jpg"
+  },
+  {
+    "revision": "07161d7b6e32ad4505493fc01b6d2626",
+    "url": "/static/media/people2.07161d7b.jpg"
+  },
+  {
+    "revision": "d56bc7959dadd716c9d31f6ca5725cdd",
+    "url": "/static/media/people3.d56bc795.jpg"
+  },
+  {
+    "revision": "6a549d6c53018685187a26dd802f5409",
+    "url": "/static/media/people4.6a549d6c.jpg"
+  },
+  {
+    "revision": "5386c510735b3d1cddb28f4fdd147444",
+    "url": "/static/media/sale.5386c510.jpg"
+  }
+]);
